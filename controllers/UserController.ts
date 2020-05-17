b9c72241e7b8cf0dd64a16b16a43bdc412215e58
sarthaksavvy <bitfumes@gmail.com>
2020-05-17 14:04:12 +0530

    user deleted from mongodb

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -13,13 +13,41 @@ export default {
     ctx.response.body = data;
   },
   async store(ctx: any) {
+    if (!ctx.request.hasBody) {
+      ctx.response.status = 400; // bad request
+      ctx.response.body = { error: "Please provide the required data" };
+      return;
+    }
     const { value } = await ctx.request.body();
+    if (!value.email) {
+      ctx.response.status = 422; // unprocessable entity
+      ctx.response.body = {
+        error: { message: "Email field is required" },
+      };
+      return;
+    }
+    if (!value.name) {
+      ctx.response.status = 422; // unprocessable entity
+      ctx.response.body = {
+        error: { message: "Name field is required" },
+      };
+      return;
+    }
+    if (!value.password) {
+      ctx.response.status = 422; // unprocessable entity
+      ctx.response.body = {
+        error: { message: "Password field is required" },
+      };
+      return;
+    }
     const insertId = await user.insertOne(value);
     ctx.response.status = 201;
     ctx.response.body = insertId;
   },
   update(ctx: any) {
   },
-  destroy(ctx: any) {
+  async destroy(ctx: any) {
+    await user.deleteOne({ _id: { $oid: ctx.params.id } });
+    ctx.response.status = 204; // no content
   },
 };

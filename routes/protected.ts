8270eac51991bf078c80d9ff73d76fc0a3458bae
sarthaksavvy <bitfumes@gmail.com>
2020-05-20 15:10:22 +0530

    show user details of auth user

--- routes/protected.ts
+++ routes/protected.ts
@@ -1,16 +1,19 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
+import { ObjectId } from "https://deno.land/x/mongo@v0.6.0/mod.ts";
 const protectedRouter = new Router();
 import token from "../util/token.ts";
+import db from "../config/databases.ts";
+const userCollection = db.collection("users");
 
-protectedRouter.get("/me", (ctx: any) => {
+protectedRouter.get("/me", async (ctx: any) => {
   const authorization = ctx.request.headers.get("authorization");
   const headerToken = authorization.replace("Bearer ", "");
   const payload = token.fetchUserId(headerToken);
   if (payload) {
-    console.log(payload.uid);
+    const uid: string = String(payload.uid);
+    const user = await userCollection.findOne({ _id: ObjectId(uid) });
+    ctx.response.body = user;
   }
-
-  ctx.response.body = "I am protected route";
 });
 
 export default protectedRouter;

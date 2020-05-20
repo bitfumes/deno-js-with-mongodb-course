2d88a5a84cebcbec24aa7b4baa36598e57bf4260
sarthaksavvy <bitfumes@gmail.com>
2020-05-20 10:07:44 +0530

    extract auth token from header

--- main.ts
+++ main.ts
@@ -9,6 +9,17 @@ const HOST = env.APP_HOST || "http://localhost";
 const PORT = +env.APP_PORT || 4000;
 
 app.use(router.routes());
+app.use(async (ctx: any, next) => {
+  const authorization = ctx.request.headers.get("authorization");
+  if (!authorization) {
+    await next();
+    return;
+  }
+  const token = authorization.replace("Bearer ", "");
+  // validate token
+
+  // handle invalide or no token
+});
 app.use(notFound);
 
 console.log(`server is started at ${HOST}:${PORT}`);
